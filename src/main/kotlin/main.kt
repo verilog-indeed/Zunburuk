3c3e4f0f969493a11e308a43f00c37f5de73c962
dhia@Kingsman <dhia.zerrari@outlook.com>
2021-04-14 13:26:45 +0100

    added a test implementation for Heun's solution for damped harmonic motion

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,21 +2,60 @@ package dz.lightyearsoftworks.zunburuk
 
 import kotlin.math.cos
 import kotlin.math.abs
+import kotlin.math.exp
 import kotlin.math.sin
-import kotlin.math.sqrt
 import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
     println("Hello World!")
     val deltaT = measureTimeMillis {
-        accuratePendulumTest()
+        dampedTest()
+        //accuratePendulumTest()
         //heunMethod()
         //eulerMethod()
     }
     println("Executed with love in $deltaT milliseconds.")
 }
 
+fun dampedTest()    {
+    //this essentially solves a system of two equations: Y' = P and P' = -aY -bP
+    //Yi+1 = Yi + 1/2 (k1 + k2) * h
+    //Pi+1 = Pi + 1/2 (u1 + u2) * h
+    val h = 0.01
+    val Y_factor = 1
+    val P_factor = 2
+    var Xi = 0.0
+    var Yi = 5.0
+    var Pi = 0.0
+    var k1: Double
+    var k2: Double
+    var u1: Double
+    var u2: Double
+
+    for (i in 1..1000)   {
+        println("Y($Xi) = $Yi")
+        /*val exactYi = 5 * exp(-0.125 * Xi) * cos (0.992*Xi)
+        println("exact Yi = $exactYi")
+        val dev = (abs(Yi - exactYi)/exactYi) * 100
+        println("deviation is $dev%")*/
+
+        k1 = Pi
+        u1 = -Y_factor * Yi - P_factor * Pi
+        k2 = Pi + h * u1
+        u2 = -Y_factor * (Yi + h * k1) - P_factor * (Pi + h * u1)
+
+        //advancing by one step
+        Xi += h
+        Yi += 0.5 * (k1 + k2) * h
+        Pi += 0.5 * (u1 + u2) * h
+    }
+}
+
 fun accuratePendulumTest()  {
+    //this essentially solves a system of two equations: Y' = P and P' = -aSin(Y)
+    //Yi+1 = Yi + 1/2 (k1 + k2) * h
+    //Pi+1 = Pi + 1/2 (u1 + u2) * h
+
     val h = 0.00628
     val Y_factor = 100
     var Xi = 0.0
