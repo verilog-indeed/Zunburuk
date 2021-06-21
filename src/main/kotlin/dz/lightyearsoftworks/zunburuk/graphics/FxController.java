3181c0101d32edf9993af8053ec8aecd8f237851
dhia@Kingsman <dhia.zerrari@outlook.com>
2021-06-21 13:11:58 +0100

    added javafx files to begin implementing GUI

---
+++ src/main/kotlin/dz/lightyearsoftworks/zunburuk/graphics/FxController.java
@@ -0,0 +1,4 @@
+package dz.lightyearsoftworks.zunburuk.graphics;
+
+public class FxController {
+}
---
+++ src/main/kotlin/dz/lightyearsoftworks/zunburuk/graphics/FxMain.java
@@ -0,0 +1,12 @@
+package dz.lightyearsoftworks.zunburuk.graphics;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+public class FxMain extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
