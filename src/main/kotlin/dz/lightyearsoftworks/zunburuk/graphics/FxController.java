baccaf247afafb0e7ee844735673b09bf594fce4
dhia@Kingsman <dhia.zerrari@outlook.com>
2021-06-30 21:28:19 +0100

    Test implementation of simple pendulum mode

--- src/main/kotlin/dz/lightyearsoftworks/zunburuk/graphics/FxController.java
+++ src/main/kotlin/dz/lightyearsoftworks/zunburuk/graphics/FxController.java
@@ -3,12 +3,16 @@ package dz.lightyearsoftworks.zunburuk.graphics;
 import dz.lightyearsoftworks.zunburuk.DifferentialEquationType;
 import dz.lightyearsoftworks.zunburuk.DifferentialSolver;
 import dz.lightyearsoftworks.zunburuk.EquationParameters;
+import dz.lightyearsoftworks.zunburuk.ODEDataPoint;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
@@ -29,8 +33,10 @@ public class FxController {
     public TextField gravityInputField;
     public TextField lengthInputField;
     public TextField maxMoveInputField;
+    public LineChart graphChart;
     public Canvas mainCanvas;
     public DifferentialSolver currentSystem;
+    private Timeline currentAnimation;
 
     public void onInputFieldChanged(KeyEvent actionEvent) {
         TextField source = (TextField) actionEvent.getSource();
@@ -51,33 +57,66 @@ public class FxController {
             return;
         }
 
-        mainCanvas.widthProperty().bind(((Pane)mainCanvas.getParent()).widthProperty());
-        mainCanvas.heightProperty().bind(((Pane)mainCanvas.getParent()).heightProperty());
+        double maxAngle, gravity, tetherLength;
 
         try {
-            currentSystem = new DifferentialSolver(DifferentialEquationType.ORDER2_PENDULUM,
-                    new EquationParameters(Double.parseDouble(maxMoveInputField.getText()),
-                            0,
-                            Double.parseDouble(gravityInputField.getText()) / Double.parseDouble(lengthInputField.getText()),
-                            0, 0),
-                    0.0, 1.0 / 60.0);
+            maxAngle = Double.parseDouble(maxMoveInputField.getText());
+            gravity = Double.parseDouble(gravityInputField.getText());
+            tetherLength = Double.parseDouble(lengthInputField.getText());
         } catch (NumberFormatException e)   {
             System.out.println("fix your regex, 7mar");
             return;
         }
 
-        redrawSimulationObject = () -> {
+        if (currentAnimation != null)   {
+            currentAnimation.stop();
+            graphChart.getData().clear();
+        }
 
+        XYChart.Series fxData = new XYChart.Series();
+        graphChart.getData().add(fxData);
+
+        mainCanvas.widthProperty().bind(((Pane)mainCanvas.getParent()).widthProperty());
+        mainCanvas.heightProperty().bind(((Pane)mainCanvas.getParent()).heightProperty());
+
+
+        currentSystem = new DifferentialSolver(DifferentialEquationType.ORDER2_PENDULUM,
+                        new EquationParameters(maxAngle,
+                            0,
+                            gravity / tetherLength,
+                            0, 0),
+                    0.0, 1.0 / 60.0);
+
+        updateChart = (dp, series) -> {
+            series.getData().add(new XYChart.Data<>(dp.getT(), dp.getY()));
+        };
+
+        redrawSimulationObject = (dp) -> {
+            GraphicsContext picassoThePainter = mainCanvas.getGraphicsContext2D();
+            double width = mainCanvas.getWidth(); double height = mainCanvas.getHeight();
+            picassoThePainter.setLineWidth(3.0);
+            double currentX = width/2.0 + 100 * tetherLength * Math.sin(dp.getY());
+            double currentY = 60 + 100 * tetherLength * Math.cos(dp.getY());
+            picassoThePainter.strokeLine(width/2.0,
+                                        60,
+                                        currentX,
+                                        currentY);
+            picassoThePainter.setFill(Color.INDIANRED);
+            picassoThePainter.fillOval(currentX - 15.0,currentY - 15.0,30.0,30.0);
+            picassoThePainter.strokeOval(currentX - 15.0,currentY - 15.0,30.0,30.0);
+            picassoThePainter.setLineWidth(1.0);
         };
 
         EventHandler<ActionEvent> simulationSteppingHandler = event -> {
+            ODEDataPoint dp = currentSystem.nextDataPoint();
             redrawCanvas();
-            redrawSimulationObject.draw();
+            redrawSimulationObject.draw(dp);
+            updateChart.update(dp, fxData);
         };
 
-        Timeline simulation = new Timeline(new KeyFrame(Duration.millis(1000.0/60.0), simulationSteppingHandler));
-        simulation.setCycleCount(Timeline.INDEFINITE);
-        simulation.play();
+        currentAnimation = new Timeline(new KeyFrame(Duration.seconds(1/60.0), simulationSteppingHandler));
+        currentAnimation.setCycleCount(Timeline.INDEFINITE);
+        currentAnimation.play();
     }
 
     private void redrawCanvas() {
@@ -87,13 +126,21 @@ public class FxController {
         //draws a nice lil rectangular base
         picassoThePainter.setFill(Color.GRAY);
         picassoThePainter.fillRect(width/2.0 - 50.0, 40.0, 100.0, 20.0);
+        picassoThePainter.setLineWidth(2.0);
         picassoThePainter.setStroke(Color.BLACK);
         picassoThePainter.strokeRect(width/2.0 - 50.0, 40.0, 100.0, 20.0);
+        picassoThePainter.setLineWidth(1.0);
     }
 
     SimulationSpecificRedraw redrawSimulationObject;
+    SimulationSpecificChart updateChart;
+
+    private interface SimulationSpecificChart {
+        public abstract void update(ODEDataPoint currentState, XYChart.Series dataSeries);
+    }
+
     private interface SimulationSpecificRedraw {
-        public abstract void draw();
+        public abstract void draw(ODEDataPoint currentState);
     }
 
 }
--- src/main/kotlin/dz/lightyearsoftworks/zunburuk/graphics/FxMain.java
+++ src/main/kotlin/dz/lightyearsoftworks/zunburuk/graphics/FxMain.java
@@ -13,66 +13,20 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class FxMain extends Application {
-    /*@Override
-    public void start(Stage primaryStage) {
-        //Parent root = FXMLLoader.load(getClass().getResource("main_stylesheet.fxml"));
-        primaryStage.setTitle("Flamingo!");
-
-        DifferentialSolver oscillatorSystem = new DifferentialSolver(DifferentialEquationType.ORDER2_UNDAMPED,
-                        new EquationParameters(1, 0, 1
-                                                , 0, 0),
-                        0.0, 0.017);
-
-
-        NumberAxis timeAxis = new NumberAxis();
-        timeAxis.setLabel("Time");
-
-        NumberAxis motionAxis = new NumberAxis();
-        motionAxis.setLabel("Y(t)");
-
-        LineChart graph = new LineChart(timeAxis, motionAxis);
-        XYChart.Series fxData = new XYChart.Series();
-        fxData.setName("cos(t)");
-
-        graph.getData().add(fxData);
-
-        VBox vbox = new VBox(graph);
-
-        Scene first = new Scene(vbox,640, 480, Color.LIGHTPINK);
-        primaryStage.setScene(first);
-        primaryStage.show();
-
-        EventHandler<ActionEvent> chartUpdater = event -> {
-            ODEDataPoint nextValue = oscillatorSystem.nextDataPoint();
-            fxData.getData().add(
-                    new XYChart.Data<>(nextValue.getT(), nextValue.getY())
-            );
-        };
-        Timeline updateChart = new Timeline(new KeyFrame(Duration.millis(17), chartUpdater));
-        updateChart.setCycleCount(Timeline.INDEFINITE);
-        updateChart.play();
-    }*/
-
-    public static void main(String[] args)  {
-        launch(args);
-    }
 
     @Override
     public void start(Stage pStage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("main_stylesheet.fxml"));
         loader.load();
         Parent root = loader.getRoot();
-        /*
-        * Perhaps no need to update everytime window is resized
-        * if its going to update itself on every animation frame anyway?
-        *
-        canvas.widthProperty().bind(((Pane)canvas.getParent()).widthProperty());
-        canvas.heightProperty().bind(((Pane)canvas.getParent()).heightProperty());
-        canvas.widthProperty().addListener(evnt -> redrawCanvas());
-        canvas.heightProperty().addListener(evnt -> redrawCanvas());*/
-
+        pStage.setTitle("Zunburuk");
         Scene main = new Scene(root);
         pStage.setScene(main);
         pStage.show();
     }
+
+    public static void main(String[] args)  {
+        launch(args);
+    }
+
 }
