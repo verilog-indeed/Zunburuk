374b896ea60ffba7da04662b301f50ee9d23797a
Dorian <dhia.zerrari@outlook.com>
2021-08-06 21:51:47 +0100

    modified audio test to work with DifferentialSolver, tweaked the sampling rate

--- src/main/kotlin/dz/lightyearsoftworks/zunburuk/main.kt
+++ src/main/kotlin/dz/lightyearsoftworks/zunburuk/main.kt
@@ -1,52 +1,45 @@
 package dz.lightyearsoftworks.zunburuk
 
 import java.lang.Thread.sleep
-import javax.sound.sampled.AudioFormat
-import javax.sound.sampled.AudioInputStream
-import javax.sound.sampled.AudioSystem
-import javax.sound.sampled.LineUnavailableException
+import javax.sound.sampled.*
 import kotlin.collections.ArrayList
-import kotlin.math.cos
-import kotlin.math.sin
+import kotlin.math.*
 
+var angVel = 2 * Math.PI * 1000
+//minfreq = 70Hz?
+//maxfreq = 14,000Hz?
+//I dont know why, but this is the best tradeoff between sampling rate/nbr of samples and audio quality
+var samplingRate = 48000.0F
+var numberOfSamples = 6000
 fun main(args: Array<String>) {
 
-    println("Hello World!")
+    println("Hello World! $numberOfSamples")
     var function: ArrayList<ODEDataPoint> = ArrayList()
-
     var currentSystem = DifferentialSolver(DifferentialEquationType.ORDER2_UNDAMPED,
                                             EquationParameters(10.0, 0.0,
-                                                                39478418.0,0.0, 0.0),
-                                            0.0, 1.0 / 44100000000.0)
-    for (i in 0..441000)   {
+                                                                angVel * angVel,0.0, 0.0),
+                                            0.0, 1.0 / (samplingRate * 500.0))
+    for (i in 0..numberOfSamples)   {
         function.add(currentSystem.nextDataPoint())
-        for (j in 0..999999) {
+        for (j in 1..499) {
             currentSystem.nextDataPoint()
         }
     }
-
-    var Xi = 0.0
-    /*for (i in 0..441000)    {
-        function.add(ODEDataPoint(Xi, 1.0 * cos(2 * Math.PI * 300 * Xi)))
-        Xi += 1.0 / 44100.0
-    }*/
     audioTest(function)
 }
 
 fun audioTest(function: ArrayList<ODEDataPoint>) {
-    //val strem = AudioInputStream(FunctionalInputStream(function, 10.0),
-                                //AudioFormat(44100.0F, 16, 1, true, false),
-                                    //441000)
-    val strem = AudioInputStream(FunctionalInputStream(function,1.0),
-                AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100.0F, 16, 1, 2, 88200.0F, false),
-                441000)
+    val strem = AudioInputStream(FunctionalInputStream(function, 10.0),
+                                AudioFormat(samplingRate, 16, 1, true, false),
+                        numberOfSamples.toLong())
     val clippy = AudioSystem.getClip()
     try {
         clippy.open(strem)
+        clippy.loop(Clip.LOOP_CONTINUOUSLY)
         clippy.start()
         sleep(2000)
         println("2 secs")
-        sleep(9000) //need to wait for the sound to play in a background thread
+        sleep(2500) //need to wait for the sound to play in a background thread
     } catch (e: LineUnavailableException)    {
 
     } finally {
